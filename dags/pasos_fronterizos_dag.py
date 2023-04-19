1765dfe47579e614c3b1e67903279bb41d1def70
jmartinelena <jmartinelena1@gmail.com>
2023-04-19 13:28:29 -0300

    Agrego task para crear tabla de pasos

--- dags/pasos_fronterizos_dag.py
+++ dags/pasos_fronterizos_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
 
 default_args = {
     'owner': 'Juan Martin Elena',
@@ -17,4 +18,23 @@ with DAG(
     start_date=datetime(2023,4,19),
     schedule='@hourly'
 ) as dag:
-    pass
+    creacion_tabla = MySqlOperator(
+        task_id = 'creacion_tabla',
+        mysql_conn_id = 'mysql_pasos',
+        sql = """
+            CREATE TABLE IF NOT EXISTS pasos_fronterizos(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                paso VARCHAR(100) NOT NULL,
+                pais VARCHAR(20) NOT NULL,
+                provincia VARCHAR(20) NOT NULL,
+                estado VARCHAR(20) NOT NULL,
+                tipo VARCHAR(20) NOT NULL,
+                temperatura INT,
+                tiempo VARCHAR(100),
+                viento VARCHAR(100),
+                visibilidad VARCHAR(100),
+                altura_del_rio INT,
+                alerta_del_rio INT,
+                evacuacion_del_rio INT
+            );"""
+    )
