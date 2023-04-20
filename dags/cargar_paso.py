c9427ea5257ffe8510eb6ff745c01f014802afd7
jmartinelena <jmartinelena1@gmail.com>
2023-04-20 03:15:07 -0300

    Agrego commit a la transaccion y modifico executes

--- dags/cargar_paso.py
+++ dags/cargar_paso.py
@@ -7,22 +7,27 @@ def cargar_paso(id, ti=None):
     hook = MySqlHook(mysql_conn_id='mysql_pasos')
     with hook.get_conn() as conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"""
-                DELETE FROM pasos_fronterizos WHERE fecha_scaneo = '{paso['fecha_scaneo']}';""")
+            cursor.execute("""
+                DELETE FROM pasos_fronterizos WHERE fecha_scaneo = %s;""", (paso['fecha_scaneo']))
             
             if paso['tipo_de_paso'] == 'Ruta':
-                cursor.execute(f"""
+                print('ruta')
+                cursor.execute("""
                     INSERT INTO 
                         pasos_fronterizos(paso, pais, provincia, estado, tipo, fecha_scaneo, ultima_actualizacion, temperatura, tiempo, viento, visibilidad)
                     VALUES
-                        ('{paso['paso']}', '{paso['pais']}', '{paso['provincia']}', '{paso['estado']}', '{paso['tipo_de_paso']}',
-                        '{paso['fecha_scaneo']}', '{paso['ultima_actualizacion']}',
-                        '{paso['temperatura']}', '{paso['tiempo']}', '{paso['viento']}', '{paso['visibilidad']}');""")
+                        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", 
+                        (paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
+                        paso['fecha_scaneo'], paso['ultima_actualizacion'],
+                        paso['temperatura'], paso['tiempo'], paso['viento'], paso['visibilidad']))
             elif paso['tipo_de_paso'] == 'Rio':
-                cursor.execute(f"""
+                print('rio')
+                cursor.execute("""
                     INSERT INTO 
                         pasos_fronterizos(paso, pais, provincia, estado, tipo, fecha_scaneo, ultima_actualizacion, altura_del_rio, alerta_del_rio, evacuacion_del_rio)
                     VALUES
-                        ('{paso['paso']}', '{paso['pais']}', '{paso['provincia']}', '{paso['estado']}', '{paso['tipo_de_paso']}',
-                        '{paso['fecha_scaneo']}', '{paso['ultima_actualizacion']}',
-                        '{paso['altura_del_río']}', '{paso['alerta']}', '{paso['evacuación']}');""")
+                        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", 
+                        (paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
+                        paso['fecha_scaneo'], paso['ultima_actualizacion'],
+                        paso['altura_del_río'], paso['alerta'], paso['evacuación']))
+        conn.commit()
