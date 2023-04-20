8f8996eacfb4f6d39cd019fb3c7ae13b174fe002
jmartinelena <jmartinelena1@gmail.com>
2023-04-20 03:33:22 -0300

    Corrijo bug en executes y cambio where en delete

--- dags/cargar_paso.py
+++ dags/cargar_paso.py
@@ -2,32 +2,30 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 def cargar_paso(id, ti=None):
     paso = ti.xcom_pull(task_ids=id)
-    #print(paso)
+    print(paso)
     
     hook = MySqlHook(mysql_conn_id='mysql_pasos')
     with hook.get_conn() as conn:
         with conn.cursor() as cursor:
             cursor.execute("""
-                DELETE FROM pasos_fronterizos WHERE fecha_scaneo = %s;""", (paso['fecha_scaneo']))
+                DELETE FROM pasos_fronterizos WHERE fecha_scaneo = %s AND paso = %s;""", [paso['fecha_scaneo'], paso['paso']])
             
             if paso['tipo_de_paso'] == 'Ruta':
-                print('ruta')
                 cursor.execute("""
                     INSERT INTO 
                         pasos_fronterizos(paso, pais, provincia, estado, tipo, fecha_scaneo, ultima_actualizacion, temperatura, tiempo, viento, visibilidad)
                     VALUES
                         (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", 
-                        (paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
+                        [paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
                         paso['fecha_scaneo'], paso['ultima_actualizacion'],
-                        paso['temperatura'], paso['tiempo'], paso['viento'], paso['visibilidad']))
+                        paso['temperatura'], paso['tiempo'], paso['viento'], paso['visibilidad']])
             elif paso['tipo_de_paso'] == 'Rio':
-                print('rio')
                 cursor.execute("""
                     INSERT INTO 
                         pasos_fronterizos(paso, pais, provincia, estado, tipo, fecha_scaneo, ultima_actualizacion, altura_del_rio, alerta_del_rio, evacuacion_del_rio)
                     VALUES
                         (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", 
-                        (paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
+                        [paso['paso'], paso['pais'], paso['provincia'], paso['estado'], paso['tipo_de_paso'],
                         paso['fecha_scaneo'], paso['ultima_actualizacion'],
-                        paso['altura_del_río'], paso['alerta'], paso['evacuación']))
+                        paso['altura_del_río'], paso['alerta'], paso['evacuación']])
         conn.commit()
